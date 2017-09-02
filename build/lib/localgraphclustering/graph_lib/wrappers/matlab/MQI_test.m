1b305569a292003f6d3ef00601f417dcb77197bc
kfoynt <kfount@berkeley.edu>
2017-09-02 01:34:29 -0700

    First commit of LocalGraphClustering - version 0.1.6

--- build/lib/localgraphclustering/graph_lib/wrappers/matlab/MQI_test.m
+++
@@ -1,5 +0,0 @@
-A=readSMAT('../../graph/minnesota.smat');
-R=readSeed('../../graph/minnesota_R.smat');
-[actual_length,ret_set]=MQI(A,R);
-actual_length
-ret_set
--- build/lib/localgraphclustering/graph_lib/wrappers/python/test_python_wrappers.sh
+++
@@ -1,6 +0,0 @@
-python aclpagerank_test.py
-python aclpagerank_weighted_test.py
-python MQI_test.py
-python ppr_path_test.py
-python proxl1PRaccel_test.py
-python sweepcut_test.py
