d32c19e7168e352f56f9c697d0342f8ad9584a2f
kfoynt <kfount@berkeley.edu>
2017-09-02 00:09:21 -0700

    First commit of LocalGraphClustering - version 0.1.6

--- build/scripts-2.7/createGraphLibFile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd "localgraphclustering/graph_lib/lib/graph_lib_test"
-pwd
-set -e
-make clean
-make -f Makefile
-
-cp libgraph.dylib ../../../../build/lib/localgraphclustering/graph_lib/lib/graph_lib_test/
-
