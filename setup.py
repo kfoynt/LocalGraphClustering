3dd22e555047038fb7e378447f5454f6ef56e8b4
kfoynt <kfount@berkeley.edu>
2017-09-13 23:18:21 -0400

    errors in setup

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
       # Versions should comply with PEP440.  For a discussion on single-sourcing
       # the version across setup.py and the project code, see
       # https://packaging.python.org/en/latest/single_source_version.html
-      version='0.2.8',
+      version='0.3.1',
       
       description='Package for local graph clustering',
       long_description=long_description,
@@ -133,6 +133,6 @@ setup(
       #                    ],
       #},
       
-      #scripts=['bin/createGraphLibFile.sh'],
+      scripts=['bin/createGraphLibFile.sh'],
       cmdclass={'install': MyInstall},
       )
