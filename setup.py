75a49f55435c6ea78f1478f35fe514c7c66afd4e
kfoynt <kfount@berkeley.edu>
2017-12-21 16:57:57 -0800

    test

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ class MyInstall(install):
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
