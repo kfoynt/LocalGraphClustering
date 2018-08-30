79be007b2bfe2f5a6b4cca332fa2528b7a33b9ee
Meng Liu <liu1740@purdue.edu>
2018-08-30 17:45:46 -0400

    fix bugs

--- localgraphclustering/tests/test_notebooks.py
+++ localgraphclustering/tests/test_notebooks.py
@@ -5,12 +5,13 @@ import os
 
 def _exec_notebook(path):
     with tempfile.NamedTemporaryFile(suffix=".ipynb") as fout:
-        args = ["jupyter", "nbconvert", "--to", "notebook", "--execute",
+        args = ["python3","-m","jupyter", "nbconvert", "--to", "notebook", "--execute",
                 "--ExecutePreprocessor.timeout=1000", "--ExecutePreprocessor.kernel_name=python3",
                 "--output", fout.name, path]
-        subprocess.check_call(args)
+        subprocess.check_call(args,cwd="/homes/liu1740/Research/LocalGraphClustering")
 
 
 def test():
-	os.chdir("/home/travis/build/MengLiuPurdue/LocalGraphClustering")
-    _exec_notebook('notebooks/examples.ipynb')
+	os.system('cp notebooks/examples.ipynb .')
+	_exec_notebook('examples.ipynb')
+	os.system('rm examples.ipynb')
