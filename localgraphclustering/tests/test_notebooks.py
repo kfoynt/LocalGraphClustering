35e07b8913d2c852e566c4bc2b08290512c34297
Meng Liu <liumeng19952012@outlook.com>
2018-08-30 16:56:56 -0400

    fix bugs of notebook test in travis

--- localgraphclustering/tests/test_notebooks.py
+++ localgraphclustering/tests/test_notebooks.py
@@ -5,7 +5,7 @@ import tempfile
 def _exec_notebook(path):
     with tempfile.NamedTemporaryFile(suffix=".ipynb") as fout:
         args = ["jupyter", "nbconvert", "--to", "notebook", "--execute",
-                "--ExecutePreprocessor.timeout=1000",
+                "--ExecutePreprocessor.timeout=1000", "--ExecutePreprocessor.kernel_name=python3",
                 "--output", fout.name, path]
         subprocess.check_call(args)
 
