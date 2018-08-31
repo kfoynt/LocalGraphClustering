672a450eae4005fc1124ac6f43475890c16f8c80
Meng Liu <liumeng19952012@outlook.com>
2018-08-30 21:58:32 -0400

    set travis to wait longer on the tests

--- localgraphclustering/tests/test_notebooks.py
+++ localgraphclustering/tests/test_notebooks.py
@@ -6,7 +6,7 @@ import os
 def _exec_notebook(path):
     with tempfile.NamedTemporaryFile(suffix=".ipynb") as fout:
         args = ["python3","-m","jupyter", "nbconvert", "--to", "notebook", "--execute",
-                "--ExecutePreprocessor.timeout=1000", "--ExecutePreprocessor.kernel_name=python3",
+                "--ExecutePreprocessor.timeout=3000", "--ExecutePreprocessor.kernel_name=python3",
                 "--output", fout.name, path]
         subprocess.check_call(args)
 
