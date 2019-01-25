6762305555aedb8c0834e8d6938e6cad24359bf7
kfoynt <kfount@berkeley.edu>
2019-01-25 17:37:24 -0500

    changed an example

--- notebooks/NCPs_for_flow_paper.py
+++ notebooks/NCPs_for_flow_paper.py
@@ -113,95 +113,95 @@ import csv
 
 # pickle.dump(ncp_instance, open('results/' + "acl" + "-ncp-" + "colgate" + '.pickle', 'wb'))
 
-# print("Running ppimips")
+print("Running ppimips")
 
-# # Read graph. This also supports gml and graphml format.
-# # The MIPS Mammalian Protein-Protein Database is a database for protein-protein interactions of mammalian species. 
-# # We used the data set proposed in consisting of a subset of 220 protein complexes of 1562 proteins. 
-# # Details can be found here: https://clusteval.sdu.dk/1/datasets/685
-# g = lgc.GraphLocal('./datasets/ppi_mips.graphml','graphml',' ')
-# g.discard_weights()
+# Read graph. This also supports gml and graphml format.
+# The MIPS Mammalian Protein-Protein Database is a database for protein-protein interactions of mammalian species. 
+# We used the data set proposed in consisting of a subset of 220 protein complexes of 1562 proteins. 
+# Details can be found here: https://clusteval.sdu.dk/1/datasets/685
+g = lgc.GraphLocal('./datasets/ppi_mips.graphml','graphml',' ')
+g.discard_weights()
 
-# ncp_instance = lgc.NCPData(g)
-# ncp_instance.approxPageRank(ratio=0.8,timeout=5000000,nthreads=24)
+ncp_instance = lgc.NCPData(g)
+ncp_instance.approxPageRank(ratio=1,timeout=5000000,nthreads=24)
 
-# ncp_plots = lgc.NCPPlots(ncp_instance,method_name = "acl")
-# #plot conductance vs size
-# fig, ax, min_tuples = ncp_plots.cond_by_size()
-# plt.savefig('figures/cond_card_ppimips.png', bbox_inches='tight')
-# plt.show()
-# #plot conductance vs volume
-# fig, ax, min_tuples = ncp_plots.cond_by_vol()
-# plt.savefig('figures/cond_vol_ppimips.png', bbox_inches='tight')
-# plt.show()
-# #plot isoperimetry vs size
-# fig, ax, min_tuples = ncp_plots.isop_by_size()
-# plt.savefig('figures/expand_card_ppimips.png', bbox_inches='tight')
-# plt.show()
+ncp_plots = lgc.NCPPlots(ncp_instance,method_name = "acl")
+#plot conductance vs size
+fig, ax, min_tuples = ncp_plots.cond_by_size()
+plt.savefig('figures/cond_card_ppimips.png', bbox_inches='tight')
+plt.show()
+#plot conductance vs volume
+fig, ax, min_tuples = ncp_plots.cond_by_vol()
+plt.savefig('figures/cond_vol_ppimips.png', bbox_inches='tight')
+plt.show()
+#plot isoperimetry vs size
+fig, ax, min_tuples = ncp_plots.isop_by_size()
+plt.savefig('figures/expand_card_ppimips.png', bbox_inches='tight')
+plt.show()
 
-# pickle.dump(ncp_instance, open('results/' + "acl" + "-ncp-" + "ppimips" + '.pickle', 'wb'))
+pickle.dump(ncp_instance, open('results/' + "acl" + "-ncp-" + "ppimips" + '.pickle', 'wb'))
 
-# print("Running sfld")
+print("Running sfld")
 
-# # Read graph. This also supports gml and graphml format.
-# # The data set contains pairwise similarities of blasted 
-# # sequences of 232 proteins belonging to the amidohydrolase superfamily. 
-# g = lgc.GraphLocal('./datasets/sfld_brown_et_al_amidohydrolases_protein_similarities_for_beh.graphml','graphml',' ')
-# g.discard_weights()
+# Read graph. This also supports gml and graphml format.
+# The data set contains pairwise similarities of blasted 
+# sequences of 232 proteins belonging to the amidohydrolase superfamily. 
+g = lgc.GraphLocal('./datasets/sfld_brown_et_al_amidohydrolases_protein_similarities_for_beh.graphml','graphml',' ')
+g.discard_weights()
 
-# ncp_instance = lgc.NCPData(g)
-# ncp_instance.approxPageRank(ratio=0.8,timeout=5000000,nthreads=24)
+ncp_instance = lgc.NCPData(g)
+ncp_instance.approxPageRank(ratio=1,timeout=5000000,nthreads=24)
 
-# ncp_plots = lgc.NCPPlots(ncp_instance,method_name = "acl")
-# #plot conductance vs size
-# fig, ax, min_tuples = ncp_plots.cond_by_size()
-# plt.savefig('figures/cond_card_sfld.png', bbox_inches='tight')
-# plt.show()
-# #plot conductance vs volume
-# fig, ax, min_tuples = ncp_plots.cond_by_vol()
-# plt.savefig('figures/cond_vol_sfld.png', bbox_inches='tight')
-# plt.show()
-# #plot isoperimetry vs size
-# fig, ax, min_tuples = ncp_plots.isop_by_size()
-# plt.savefig('figures/expand_card_sfld.png', bbox_inches='tight')
-# plt.show()
+ncp_plots = lgc.NCPPlots(ncp_instance,method_name = "acl")
+#plot conductance vs size
+fig, ax, min_tuples = ncp_plots.cond_by_size()
+plt.savefig('figures/cond_card_sfld.png', bbox_inches='tight')
+plt.show()
+#plot conductance vs volume
+fig, ax, min_tuples = ncp_plots.cond_by_vol()
+plt.savefig('figures/cond_vol_sfld.png', bbox_inches='tight')
+plt.show()
+#plot isoperimetry vs size
+fig, ax, min_tuples = ncp_plots.isop_by_size()
+plt.savefig('figures/expand_card_sfld.png', bbox_inches='tight')
+plt.show()
 
-# pickle.dump(ncp_instance, open('results/' + "acl" + "-ncp-" + "sfld" + '.pickle', 'wb'))
+pickle.dump(ncp_instance, open('results/' + "acl" + "-ncp-" + "sfld" + '.pickle', 'wb'))
 
-mygraphs = {'email-Enron':'/u4/kfountoulakis/flowReviewPaper/LocalGraphClustering/notebooks/datasets/email-Enron.edgelist',
-            'pokec':'/u4/kfountoulakis/flowReviewPaper/LocalGraphClustering/notebooks/datasets/soc-pokec-relationships.edgelist',
-            'orkut':'/u4/kfountoulakis/flowReviewPaper/LocalGraphClustering/notebooks/datasets/com-orkut.ungraph.edgelist',
-            'livejournal':'/u4/kfountoulakis/flowReviewPaper/LocalGraphClustering/notebooks/datasets/soc-LiveJournal1.edgelist'
-           }
+# mygraphs = {'email-Enron':'/u4/kfountoulakis/flowReviewPaper/LocalGraphClustering/notebooks/datasets/email-Enron.edgelist',
+#             'pokec':'/u4/kfountoulakis/flowReviewPaper/LocalGraphClustering/notebooks/datasets/soc-pokec-relationships.edgelist',
+#             'orkut':'/u4/kfountoulakis/flowReviewPaper/LocalGraphClustering/notebooks/datasets/com-orkut.ungraph.edgelist',
+#             'livejournal':'/u4/kfountoulakis/flowReviewPaper/LocalGraphClustering/notebooks/datasets/soc-LiveJournal1.edgelist'
+#            }
 
-start = time.time()
-for (gname,gfile) in mygraphs.items():
-    print(gname, gfile)
-    sep = ' '
-    if isinstance(gfile, tuple):
-        sep = gfile[1]
-        gfile = gfile[0]
+# start = time.time()
+# for (gname,gfile) in mygraphs.items():
+#     print(gname, gfile)
+#     sep = ' '
+#     if isinstance(gfile, tuple):
+#         sep = gfile[1]
+#         gfile = gfile[0]
         
-    print("Running " + gname)
+#     print("Running " + gname)
     
-    g = lgc.GraphLocal(os.path.join("..", "data", gfile),'edgelist', "	")
-    g.discard_weights()
+#     g = lgc.GraphLocal(os.path.join("..", "data", gfile),'edgelist', "	")
+#     g.discard_weights()
 
-    ncp_instance = lgc.NCPData(g)
-    ncp_instance.approxPageRank(ratio=0.3,timeout=5000000,nthreads=24)
+#     ncp_instance = lgc.NCPData(g)
+#     ncp_instance.approxPageRank(ratio=0.3,timeout=5000000,nthreads=24)
 
-    ncp_plots = lgc.NCPPlots(ncp_instance,method_name = "acl")
-    #plot conductance vs size
-    fig, ax, min_tuples = ncp_plots.cond_by_size()
-    plt.savefig('figures/cond_card_' + gname + '.png', bbox_inches='tight')
-    plt.show()
-    #plot conductance vs volume
-    fig, ax, min_tuples = ncp_plots.cond_by_vol()
-    plt.savefig('figures/cond_vol_' + gname + '.png', bbox_inches='tight')
-    plt.show()
-    #plot isoperimetry vs size
-    fig, ax, min_tuples = ncp_plots.isop_by_size()
-    plt.savefig('figures/expand_card_' + gname + '.png', bbox_inches='tight')
-    plt.show()
+#     ncp_plots = lgc.NCPPlots(ncp_instance,method_name = "acl")
+#     #plot conductance vs size
+#     fig, ax, min_tuples = ncp_plots.cond_by_size()
+#     plt.savefig('figures/cond_card_' + gname + '.png', bbox_inches='tight')
+#     plt.show()
+#     #plot conductance vs volume
+#     fig, ax, min_tuples = ncp_plots.cond_by_vol()
+#     plt.savefig('figures/cond_vol_' + gname + '.png', bbox_inches='tight')
+#     plt.show()
+#     #plot isoperimetry vs size
+#     fig, ax, min_tuples = ncp_plots.isop_by_size()
+#     plt.savefig('figures/expand_card_' + gname + '.png', bbox_inches='tight')
+#     plt.show()
     
-    pickle.dump(ncp_instance, open('results/' + "acl" + "-ncpNOTIMPROVE-" + gname + '.pickle', 'wb'))
+#     pickle.dump(ncp_instance, open('results/' + "acl" + "-ncpNOTIMPROVE-" + gname + '.pickle', 'wb'))
