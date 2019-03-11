1b0c2a025985a2286598a6f5cdcb5f4064b4e7ab
ChesterHu <huchufeng@g.ucla.edu>
2019-03-11 13:48:43 -0400

    update y and q

--- localgraphclustering/src/lib/proxl1PRrand.cpp
+++ localgraphclustering/src/lib/proxl1PRrand.cpp
@@ -227,7 +227,8 @@ vtype graph<vtype,itype>::proxl1PRrand(vtype num_nodes, vtype* seed, vtype num_s
     }
     //proxl1PRrand::writeTime(timeStamp, "/home/c55hu/Documents/research/experiment/output/time-rand.txt");
     //proxl1PRrand::writeLog(num_nodes, "/home/c55hu/Documents/research/experiment/output/q-rand.txt", q);
-    for (vtype i = 0; i < num_nodes; ++i) q[i] = q[i]*ds[i];
+    // update y and q
+    for (vtype i = 0; i < num_nodes; ++i) q[i] = (y[i] = q[i]) * ds[i];
     delete [] candidates;
     delete [] visited;
     return not_converged;
