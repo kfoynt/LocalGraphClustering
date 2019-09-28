5da497de53b015eb66785cec2f73fb7f27501a32
kfoynt <kfount@berkeley.edu>
2019-09-27 22:46:29 -0400

    new experiments

--- notebooks/statistical_guarantees_paper_experiments/statistical_guarantees_paper_ORKUT_experiments.py
+++ notebooks/statistical_guarantees_paper_experiments/statistical_guarantees_paper_ORKUT_experiments.py
@@ -270,9 +270,9 @@ for rr in all_clusters:
         for a in a_list:
             
             if ct_outer <= 1:
-                rho = 0.15/np.sum(g.d[rr])
+                rho = 0.1 # 0.15/np.sum(g.d[rr])
             else:
-                rho = 0.20/np.sum(g.d[rr])
+                rho = 0.1 # 0.20/np.sum(g.d[rr])
             
             output_pr_clustering = approximate_PageRank(g,ref_node,method = "l1reg-rand", epsilon=1.0e-2, rho=rho, alpha=a, cpp = True, normalize=True,normalized_objective=True)
             number_experiments += 1
